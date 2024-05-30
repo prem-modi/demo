f8d03ac0e6c412d8f985669fbb1bee4af3648863
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-30 12:07:47 +0530

    Update demo5.php

--- demo5.php
+++ demo5.php
@@ -1,11 +1,13 @@
 <?php
-echo "sszsssssssssssss";
-echo "ssss";
-echo "ssss";
 
-echo "ssss";
-echo "ssss";
-echo "ssss";
+// AWS EC2 MySQL DB Server
+$servername = "3.69.166.243sssss";
+$username = "dns0108prds";
+$password = "VMS>*yUkhbo0Ot0-->ss";
+$dbname = "dns_app_prosdsss";
 
-echo "ssss";
-?>
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
